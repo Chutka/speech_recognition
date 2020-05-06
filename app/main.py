3a1019b00bf6ed9fe63d6dc3f21433a78db4c264
Chutka <chutkanub@mail.ru>
2020-05-06 18:57:18 +0300

    Make the recognition code work

--- app/main.py
+++ app/main.py
@@ -3,21 +3,22 @@ import sys
 import argparse
 import speech_recognition as sr
 
-from recognizer.recognizer import RecognizerTypes, Recognizer
-
 # obtain audio from the microphone
 r = sr.Recognizer()
 with sr.Microphone(device_index=0) as source:
     print("Say something!")
-    audio = r.listen(source, timeout=2)
+    audio = r.listen(source)
 
-# recognize speech using Sphinx
+# recognize speech using Google Speech Recognition
 try:
-    print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+    # for testing purposes, we're just using the default API key
+    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+    # instead of `r.recognize_google(audio)`
+    print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
 except sr.UnknownValueError:
-    print("Sphinx could not understand audio")
+    print("Google Speech Recognition could not understand audio")
 except sr.RequestError as e:
-    print("Sphinx error; {0}".format(e))
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
 
 # if __name__ == '__main__':
