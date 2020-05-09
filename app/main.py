719cc5bb866b12b4a8788bfd372e14f350d0a5dd
Chutka <chutkanub@mail.ru>
2020-05-09 14:23:28 +0300

    Поправил функционал. Добавил флаги. Отрефакторил все что можно

--- app/main.py
+++ app/main.py
@@ -1,26 +1,66 @@
-import os
-import sys
 import argparse
-import speech_recognition as sr
 
-# obtain audio from the microphone
-r = sr.Recognizer()
-with sr.Microphone(device_index=0) as source:
-    print("Say something!")
-    audio = r.listen(source)
+from recognizer.recognizer import Recognizer, RecognizerTypes
 
-# recognize speech using Google Speech Recognition
-try:
-    # for testing purposes, we're just using the default API key
-    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-    # instead of `r.recognize_google(audio)`
-    print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter,
+        description='''\
+Аргументы командной строки, для использования программы
+----------------------''',
+        epilog='''
+----------------------'''
+    )
+    ap.add_argument(
+        '--type',
+        '-t',
+        type=str,
+        help='''Поля тип используемого средства для распознавания: GOOGLE_SPEECH_RECOGNITION значение по умолчанию
+Возможные значения:
+- Sphinx
+- Google Speech Recognition
+- Google Cloud Speech
+- Wit.ai
+- Microsoft Bing Voice Recognition
+- Houndify
+- IBM Speech to Text
+''',
+        default=RecognizerTypes.GOOGLE_SPEECH_RECOGNITION.value
+    )
+    ap.add_argument(
+        '--device_index',
+        type=int,
+        help='Индекс используемого микрофона. По умолчанию стоит 0',
+        default=0,
+    )
+    ap.add_argument(
+        '--file',
+        '-f',
+        help='''Имя файла. Если передать, то текст будет записан в файл, иначе просто будет выведен в консоль''',
+        default=None
+    )
+    ap.add_argument(
+        '--audio',
+        type=str,
+        help='''Имя файла аудио, из которого будет производить распознавание речи.''',
+        default=None
+    )
+    args = ap.parse_args()
 
+    r = Recognizer(RecognizerTypes(args.type), device_index=args.device_index)
 
-# if __name__ == '__main__':
-  # ap = argparse.ArgumentParser()
-  # ap.add_argument()
+    if args.audio:
+        audio = r.from_file(args.audio)
+    else:
+        audio = r.listen()
+
+    recognized_text = None
+    if audio:
+        recognized_text = r.recognition(audio)
+        print(recognized_text)
+        if args.file is not None:
+            with open(args.file, 'w') as file:
+                print('Результат программы записан в файл - {}'.format(args.file))
+                file.write(recognized_text)
+    else:
+        print('Не удалось записать аудио')
--- app/recognizer/recognizer.py
+++ app/recognizer/recognizer.py
@@ -2,113 +2,141 @@ import speech_recognition as sr
 
 from enum import Enum
 
+
 class RecognizerTypes(Enum):
-  SPHINX = 'Sphinx'
-  GOOGLE_SPEECH_RECOGNITION = 'Google Speech Recognition'
-  GOOGLE_CLOUD_SPEECH = 'Google Cloud Speech'
-  WIT_AI_KEY = 'Wit.ai'
-  MICROSOFT_BING_VOICE_RECOGNITION = 'Microsoft Bing Voice Recognition'
-  HOUNDIFY = 'Houndify'
-  IBM_SPEECH_TO_TEXT = 'IBM Speech to Text'
+    SPHINX = 'Sphinx'
+    GOOGLE_SPEECH_RECOGNITION = 'Google Speech Recognition'
+    GOOGLE_CLOUD_SPEECH = 'Google Cloud Speech'
+    WIT_AI_KEY = 'Wit.ai'
+    MICROSOFT_BING_VOICE_RECOGNITION = 'Microsoft Bing Voice Recognition'
+    HOUNDIFY = 'Houndify'
+    IBM_SPEECH_TO_TEXT = 'IBM Speech to Text'
+
 
 class Recognizer:
-  def __init__(self, type):
-    if not type:
-      raise Exception('Should add type of recognizer')
-    self.recognizer = sr.Recognizer()
-    try:
-      RecognizerTypes(type)
-    except ValueError as vr:
-      raise ValueError('Type is not correct', *vr.args)
-    self.type = type
+    def __init__(self, type, device_index=0):
+        if not type:
+            raise Exception('Should add type of recognizer')
+        self.recognizer = sr.Recognizer()
+        try:
+            RecognizerTypes(type)
+        except ValueError as vr:
+            raise ValueError('Type is not correct', *vr.args)
+        self.type = type
+        if device_index is None:
+            raise Exception('Device is not defined')
+        self.device_index = device_index
 
-  def recognition(self, audio, *args, **kwargs):
-    if self.type == RecognizerTypes.SPHINX:
-      return self._sphrinx(audio, *args, **kwargs)
-    if self.type == RecognizerTypes.GOOGLE_SPEECH_RECOGNITION:
-      return self._google_speech(audio, *args, **kwargs)
-    if self.type == RecognizerTypes.GOOGLE_CLOUD_SPEECH:
-      return self._google_cloud_speech(audio, *args, **kwargs)
-    if self.type == RecognizerTypes.WIT_AI_KEY:
-      return self._wit_ai_key(audio, *args, **kwargs)
-    if self.type == RecognizerTypes.MICROSOFT_BING_VOICE_RECOGNITION:
-      return self._microsoft_bing(audio, *args, **kwargs)
-    if self.type == RecognizerTypes.HOUNDIFY:
-      return self._houndify(audio, *args, **kwargs)
-    if self.type == RecognizerTypes.IBM_SPEECH_TO_TEXT:
-      return self._ibm(audio, *args, **kwargs)
+    def listen(self):
+        with sr.Microphone(device_index=self.device_index) as source:
+            print('Начните что-то говорить')
+            audio = self.recognizer.listen(source)
+        print('Запись закончена')
+        return audio
 
-  def _sphrinx(self, audio):
-    # recognize speech using Sphinx
-    try:
-      print("Sphinx thinks you said " + self.recognizer.recognize_sphinx(audio))
-    except sr.UnknownValueError:
-      print("Sphinx could not understand audio")
-    except sr.RequestError as e:
-      print("Sphinx error; {0}".format(e))
+    def from_file(self, file_name):
+        with sr.AudioFile(file_name) as source:
+            print('Из аудио файла - {}'.format(file_name))
+            audio = self.recognizer.record(source)
+        print('Считывание закончено')
+        return audio
 
-  def _google_speech(self, audio):
-    # recognize speech using Google Speech Recognition
-    try:
-      # for testing purposes, we're just using the default API key
-      # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-      # instead of `r.recognize_google(audio)`
-      print("Google Speech Recognition thinks you said " + self.recognizer.recognize_google(audio))
-    except sr.UnknownValueError:
-      print("Google Speech Recognition could not understand audio")
-    except sr.RequestError as e:
-      print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    def recognition(self, audio, *args, **kwargs):
+        begin_text = 'Распознавание ведется с помощью - {}'
+        recognition_text = None
+        if self.type == RecognizerTypes.SPHINX:
+            print(begin_text.format(RecognizerTypes.SPHINX.value))
+            recognition_text = self._sphrinx(audio, *args, **kwargs)
+        if self.type == RecognizerTypes.GOOGLE_SPEECH_RECOGNITION:
+            print(begin_text.format(RecognizerTypes.GOOGLE_SPEECH_RECOGNITION.value))
+            recognition_text = self._google_speech(audio, *args, **kwargs)
+        if self.type == RecognizerTypes.GOOGLE_CLOUD_SPEECH:
+            print(begin_text.format(RecognizerTypes.GOOGLE_CLOUD_SPEECH.value))
+            recognition_text = self._google_cloud_speech(audio, *args, **kwargs)
+        if self.type == RecognizerTypes.WIT_AI_KEY:
+            print(begin_text.format(RecognizerTypes.WIT_AI_KEY.value))
+            recognition_text = self._wit_ai_key(audio, *args, **kwargs)
+        if self.type == RecognizerTypes.MICROSOFT_BING_VOICE_RECOGNITION:
+            print(begin_text.format(RecognizerTypes.MICROSOFT_BING_VOICE_RECOGNITION.value))
+            recognition_text = self._microsoft_bing(audio, *args, **kwargs)
+        if self.type == RecognizerTypes.HOUNDIFY:
+            print(begin_text.format(RecognizerTypes.HOUNDIFY.value))
+            recognition_text = self._houndify(audio, *args, **kwargs)
+        if self.type == RecognizerTypes.IBM_SPEECH_TO_TEXT:
+            print(begin_text.format(RecognizerTypes.IBM_SPEECH_TO_TEXT.value))
+            recognition_text = self._ibm(audio, *args, **kwargs)
+        return recognition_text
 
-  def _google_cloud_speech(self, audio):
-    # recognize speech using Google Cloud Speech
-    GOOGLE_CLOUD_SPEECH_CREDENTIALS = r"""INSERT THE CONTENTS OF THE GOOGLE CLOUD SPEECH JSON CREDENTIALS FILE HERE"""
-    try:
-      print("Google Cloud Speech thinks you said " + self.recognizer.recognize_google_cloud(audio, credentials_json=GOOGLE_CLOUD_SPEECH_CREDENTIALS))
-    except sr.UnknownValueError:
-      print("Google Cloud Speech could not understand audio")
-    except sr.RequestError as e:
-      print("Could not request results from Google Cloud Speech service; {0}".format(e))
+    def _sphrinx(self, audio):
+        # recognize speech using Sphinx
+        try:
+            return self.recognizer.recognize_sphinx(audio)
+        except sr.UnknownValueError:
+            print("Sphinx could not understand audio")
+        except sr.RequestError as e:
+            print("Sphinx error; {0}".format(e))
 
-  def _wit_ai_key(self, audio):
-    # recognize speech using Wit.ai
-    WIT_AI_KEY = "INSERT WIT.AI API KEY HERE"  # Wit.ai keys are 32-character uppercase alphanumeric strings
-    try:
-      print("Wit.ai thinks you said " + self.recognizer.recognize_wit(audio, key=WIT_AI_KEY))
-    except sr.UnknownValueError:
-      print("Wit.ai could not understand audio")
-    except sr.RequestError as e:
-      print("Could not request results from Wit.ai service; {0}".format(e))
+    def _google_speech(self, audio):
+        # recognize speech using Google Speech Recognition
+        try:
+            # for testing purposes, we're just using the default API key
+            # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+            # instead of `r.recognize_google(audio)`
+            return self.recognizer.recognize_google(audio)
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
-  def _microsoft_bing(self, audio):
-    # recognize speech using Microsoft Bing Voice Recognition
-    BING_KEY = "INSERT BING API KEY HERE"  # Microsoft Bing Voice Recognition API keys 32-character lowercase hexadecimal strings
-    try:
-      print("Microsoft Bing Voice Recognition thinks you said " + self.recognizer.recognize_bing(audio, key=BING_KEY))
-    except sr.UnknownValueError:
-      print("Microsoft Bing Voice Recognition could not understand audio")
-    except sr.RequestError as e:
-      print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
+    def _google_cloud_speech(self, audio):
+        # recognize speech using Google Cloud Speech
+        GOOGLE_CLOUD_SPEECH_CREDENTIALS = r"""INSERT THE CONTENTS OF THE GOOGLE CLOUD SPEECH JSON CREDENTIALS FILE HERE"""
+        try:
+            return self.recognizer.recognize_google_cloud(audio,
+                                                          credentials_json=GOOGLE_CLOUD_SPEECH_CREDENTIALS)
+        except sr.UnknownValueError:
+            print("Google Cloud Speech could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Cloud Speech service; {0}".format(e))
 
-  def _houndify(self, audio):
-    # recognize speech using Houndify
-    HOUNDIFY_CLIENT_ID = "INSERT HOUNDIFY CLIENT ID HERE"  # Houndify client IDs are Base64-encoded strings
-    HOUNDIFY_CLIENT_KEY = "INSERT HOUNDIFY CLIENT KEY HERE"  # Houndify client keys are Base64-encoded strings
-    try:
-      print("Houndify thinks you said " + self.recognizer.recognize_houndify(audio, client_id=HOUNDIFY_CLIENT_ID, client_key=HOUNDIFY_CLIENT_KEY))
-    except sr.UnknownValueError:
-      print("Houndify could not understand audio")
-    except sr.RequestError as e:
-      print("Could not request results from Houndify service; {0}".format(e))
+    def _wit_ai_key(self, audio):
+        # recognize speech using Wit.ai
+        WIT_AI_KEY = "INSERT WIT.AI API KEY HERE"  # Wit.ai keys are 32-character uppercase alphanumeric strings
+        try:
+            return self.recognizer.recognize_wit(audio, key=WIT_AI_KEY)
+        except sr.UnknownValueError:
+            print("Wit.ai could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Wit.ai service; {0}".format(e))
 
-  def _ibm(self, audio):
-    # recognize speech using IBM Speech to Text
-    IBM_USERNAME = "INSERT IBM SPEECH TO TEXT USERNAME HERE"  # IBM Speech to Text usernames are strings of the form XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
-    IBM_PASSWORD = "INSERT IBM SPEECH TO TEXT PASSWORD HERE"  # IBM Speech to Text passwords are mixed-case alphanumeric strings
-    try:
-      print("IBM Speech to Text thinks you said " + self.recognizer.recognize_ibm(audio, username=IBM_USERNAME, password=IBM_PASSWORD))
-    except sr.UnknownValueError:
-      print("IBM Speech to Text could not understand audio")
-    except sr.RequestError as e:
-      print("Could not request results from IBM Speech to Text service; {0}".format(e))
+    def _microsoft_bing(self, audio):
+        # recognize speech using Microsoft Bing Voice Recognition
+        BING_KEY = "INSERT BING API KEY HERE"  # Microsoft Bing Voice Recognition API keys 32-character lowercase hexadecimal strings
+        try:
+            return self.recognizer.recognize_bing(audio, key=BING_KEY)
+        except sr.UnknownValueError:
+            print("Microsoft Bing Voice Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
 
+    def _houndify(self, audio):
+        # recognize speech using Houndify
+        HOUNDIFY_CLIENT_ID = "INSERT HOUNDIFY CLIENT ID HERE"  # Houndify client IDs are Base64-encoded strings
+        HOUNDIFY_CLIENT_KEY = "INSERT HOUNDIFY CLIENT KEY HERE"  # Houndify client keys are Base64-encoded strings
+        try:
+            return self.recognizer.recognize_houndify(audio, client_id=HOUNDIFY_CLIENT_ID, client_key=HOUNDIFY_CLIENT_KEY)
+        except sr.UnknownValueError:
+            print("Houndify could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Houndify service; {0}".format(e))
 
+    def _ibm(self, audio):
+        # recognize speech using IBM Speech to Text
+        IBM_USERNAME = "INSERT IBM SPEECH TO TEXT USERNAME HERE"  # IBM Speech to Text usernames are strings of the form XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
+        IBM_PASSWORD = "INSERT IBM SPEECH TO TEXT PASSWORD HERE"  # IBM Speech to Text passwords are mixed-case alphanumeric strings
+        try:
+            return self.recognizer.recognize_ibm(audio, username=IBM_USERNAME, password=IBM_PASSWORD)
+        except sr.UnknownValueError:
+            print("IBM Speech to Text could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from IBM Speech to Text service; {0}".format(e))
